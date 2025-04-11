975abc37227af32d2ac7b98b92b41f8ff33572cf
Izzar Suly <152323638+avezoor@users.noreply.github.com>
2025-04-12 01:53:25 +0700

    Office2010

---
+++ ofc2010.cmd
@@ -0,0 +1,91 @@
+@echo off
+cls
+color 0a
+echo    __   _  _  ____  ____  __    __  ____ 
+echo   / _\ / )( \(  __)(__  )/  \  /  \(  _ \
+echo  /    \\ \/ / ) _)  / _/(  O )(  O ))   /
+echo  \_/\_/ \__/ (____)(____)\__/  \__/(__\_)
+echo.
+echo  OFFICE 2010 ACTIVATION 
+echo  Dev By Izzar Suly Nashrudin
+pause
+
+title Activate Microsoft Office 2010 without Software - Avezoor
+cls
+color 0a
+(net session >nul 2>&1)
+if %errorLevel% GTR 0 (
+    echo.
+    echo Activation cannot proceed. Please run this script as Administrator...
+    echo.
+    goto halt
+)
+
+echo =============================================================
+echo [*] Activating Microsoft Office 2010 (No Software Needed)
+echo =============================================================
+echo.
+echo [+] Supported Editions:
+echo     - Microsoft Office Standard 2010 Volume
+echo     - Microsoft Office Professional Plus 2010 Volume
+echo.
+echo [+] Preparing environment...
+
+:: Navigate to Office14 folder
+(if exist "%ProgramFiles%\Microsoft Office\Office14\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office14")
+(if exist "%ProgramFiles(x86)%\Microsoft Office\Office14\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office14")
+
+:: Uninstall previous keys
+echo [+] Removing old product keys...
+cscript //nologo ospp.vbs /unpkey:8R6BM >nul
+cscript //nologo ospp.vbs /unpkey:H3GVB >nul
+
+:: Install valid Office 2010 volume keys
+echo [+] Installing volume license keys...
+cscript //nologo ospp.vbs /inpkey:V7QKV-4XVVR-XYV4D-F7DFM-8R6BM >nul
+cscript //nologo ospp.vbs /inpkey:VYBBJ-TRJPB-QFQRF-QFT4D-H3GVB >nul
+
+:: Try activation servers
+set i=1
+
+:server
+if %i%==1 set KMS_Sev=kms7.MSGuides.com
+if %i%==2 set KMS_Sev=kms8.MSGuides.com
+if %i%==3 set KMS_Sev=kms9.MSGuides.com
+if %i%==4 goto notsupported
+
+echo.
+echo [+] Attempt %i%: Connecting to %KMS_Sev%
+cscript //nologo ospp.vbs /sethst:%KMS_Sev% >nul
+
+echo [+] Activating Office...
+cscript //nologo ospp.vbs /act | find /i "successful" && (
+    echo.
+    echo =============================================================
+    echo [✓] Activation Successful!
+    echo -------------------------------------------------------------
+    echo Support the project by starring the repo:
+    echo https://github.com/avezoor/office-activation
+    echo =============================================================
+    echo.
+    timeout /t 7 >nul
+    explorer "https://github.com/avezoor"
+    goto halt
+) || (
+    echo [!] Activation failed with %KMS_Sev%
+    echo     Retrying with another server...
+    echo.
+    set /a i+=1
+    timeout /t 5 >nul
+    goto server
+)
+
+:notsupported
+echo.
+echo [X] Sorry, your version is not supported.
+echo.
+
+:halt
+cd %~dp0
+del %0 >nul
+pause >nul
